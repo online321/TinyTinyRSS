843d2f41ef76fb3516a1a1e06bbee47509c5c7f1
mapoboss <mapoboss@gmail.com>
2017-09-17 09:20:12 +0200

    Update config.php with right php_executable and create update.sh script to lauch in a detached shell

--- config.php
+++ config.php
@@ -54,8 +54,7 @@
 	// *** Files and directories ***
 	// *****************************
 
-	define('PHP_EXECUTABLE', '/usr/bin/php');
-	// Path to PHP *COMMAND LINE* executable, used for various command-line tt-rss 
+        define('PHP_EXECUTABLE', shell_exec("which php"));                                                                                                                      // Path to PHP *COMMAND LINE* executable, used for various command-line tt-rss 
 	// programs and update daemon. Do not try to use CGI binary here, it won't work. 
 	// If you see HTTP headers being displayed while running tt-rss scripts, 
 	// then most probably you are using the CGI binary. If you are unsure what to 
---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash                                                                                                                                                
+                                                                                                                                                               
+while true; do                                                                                                                                                 
+     php update.php --feeds                                                                                                                                    
+     sleep 120                                                                                                                                                 
+done  
